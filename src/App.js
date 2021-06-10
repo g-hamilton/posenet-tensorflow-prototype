4b597fee45b11ce4d1a4eabcf09fab688bff6b9e
g-hamilton <greghamilton1@gmail.com>
2021-06-10 09:38:35 +0100

    added styled webcam & canvas comps

--- src/App.js
+++ src/App.js
@@ -2,13 +2,40 @@ import * as tf from "@tensorflow/tfjs";
 import * as posenet from "@tensorflow-models/posenet";
 import Webcam from "react-webcam";
 
+import styled from "styled-components";
+
 import "./App.css";
+
+const Camera = styled(Webcam)`
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  z-index: 9;
+  width: 640px;
+  height: 480px;
+`;
+
+const Canvas = styled.canvas`
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+  z-index: 9;
+  width: 640px;
+  height: 480px;
+`;
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Let's Flow</p>
+        <Camera />
+        <Canvas />
       </header>
     </div>
   );
